bebcb2d4cf3ea7bddce5edc753bfbc61a09d4647
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-10-18 14:49:25 -0300

    generator e classe Factory

--- Idle Factory/Base/Factory.swift
+++ Idle Factory/Base/Factory.swift
@@ -9,22 +9,85 @@ import Foundation
 import SpriteKit
 
 
+enum GeneratorPositions: CustomStringConvertible, CaseIterable {
+    case first
+    case second
+    case third
+    case fourth
+    case fifth
+    case sixth
+    
+    var description: String {
+        switch self {
+        case .first:
+            return "first"
+        case .second:
+            return "second"
+        case .third:
+            return "third"
+        case .fourth:
+            return "fourth"
+        case .fifth:
+            return "fifth"
+        case .sixth:
+            return "sixth"
+        }
+    }
+    
+    var key: String {
+        switch self {
+        case .first:
+            return "first"
+        case .second:
+            return "second"
+        case .third:
+            return "third"
+        case .fourth:
+            return "fourth"
+        case .fifth:
+            return "fifth"
+        case .sixth:
+            return "sixth"
+        }
+    }
+    
+}
+
+func getGeneratorPositions(position: String) -> GeneratorPositions {
+    switch position {
+    case "first":
+        return .first
+    case "second":
+        return .second
+    case "third":
+        return .third
+    case "fourth":
+        return .fourth
+    case "fifth":
+        return .fifth
+    case "sixth":
+        return .sixth
+    default:
+        return .first
+    }
+}
+
 enum FactoryType: CustomStringConvertible, CaseIterable {
     case Basic, NFT
     
     var description: String {
         switch self {
-            case .NFT:
-                return NSLocalizedString("NFT", comment: "")
-            case .Basic:
-                return NSLocalizedString("Basic", comment: "")
+        case .NFT:
+            return NSLocalizedString("NFT", comment: "")
+        case .Basic:
+            return NSLocalizedString("Basic", comment: "")
             
         }
     }
     
     var key: String {
         switch self {
-            case .NFT:
+        case .NFT:
             return "NFT"
         case .Basic:
             return "Basic"
@@ -32,38 +95,39 @@ enum FactoryType: CustomStringConvertible, CaseIterable {
         }
     }
     
-    static func getFactoryType(factoryType: String) -> FactoryType {
-        switch factoryType {
-            case "NFT":
-                return .NFT
-            case "Basic":
-                return .Basic
-            default:
-                return .Basic
-        }
+}
+
+func getFactoryType(factoryType: String) -> FactoryType {
+    switch factoryType {
+    case "NFT":
+        return .NFT
+    case "Basic":
+        return .Basic
+    default:
+        return .Basic
     }
 }
 
-class Factory: SKNode, Generator  {
-        
+class Factory: Generator  {
+    
     var id: String
     var perSec: Double
     var type: FactoryType
     var energy: Int
     var resourcesArray: [String]
+    var position: GeneratorPositions
+    var node: SKSpriteNode
     
-    init(id: String,perSec: Double, resourcesArray:[String] , energy: Int, type: FactoryType){
+    init(id: String,perSec: Double, resourcesArray:[String] , energy: Int, type: String, texture: SKTexture?, position: String){
         self.id = id
         self.perSec = perSec
         self.resourcesArray = resourcesArray
         self.energy = energy
-        self.type = type
-        super.init()
+        self.type = getFactoryType(factoryType: type)
+        self.node = SKSpriteNode(texture: texture)
+        self.position = getGeneratorPositions(position: position)
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     
 }
