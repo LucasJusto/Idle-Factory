dcb1e25b0d8ce7e94b83b0223d2ef81821e61ecd
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-10-26 17:17:47 -0300

    image and buttons

--- Idle Factory/UpgradeFactorySceneController.swift
+++ Idle Factory/UpgradeFactorySceneController.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 
-class UpgradeFactorySceneController: UIViewController {
-
+class UpgradeFactorySceneController: UIViewController,  UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
     /*
     // MARK: - Navigation
