625f85e14fd4bb8fa098e40cd649b7edac3d151c
RodrigoOkido <rodrigookido@gmail.com>
2021-11-01 21:43:22 -0300

    Added marketplace scene. Storyboard, collectionView, localizable added.

--- Idle Factory/HUD/HUD Actions/GameMarketplaceScene/GameMarketplaceSceneController.swift
+++ Idle Factory/HUD/HUD Actions/GameMarketplaceScene/GameMarketplaceSceneController.swift
@@ -6,37 +6,145 @@
 //
 
 import UIKit
-import SpriteKit
 
 class GameMarketplaceSceneController: UIViewController {
     
-    // MARK: - Marketplace Scene display components
-    private(set) var sceneBackground: SKShapeNode = SKShapeNode()
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    // MARK: - MARKETPLACE HEADER
+    @IBOutlet weak var marketplaceHeaderLabel: UILabel!
+    @IBOutlet weak var mainCurrencyHeaderView: UIView!
+    @IBOutlet weak var mainCurrencyLabel: UILabel!
+    @IBOutlet weak var premiumCurrencyHeaderView: UIView!
+    @IBOutlet weak var premiumCurrencyLabel: UILabel!
+    
+    @IBOutlet weak var itemTypeSelector: UISegmentedControl!
+    @IBOutlet weak var sellAItemButton: UIButton!
+    @IBOutlet weak var myAnnouncesButton: UIButton!
+    
+    // MARK: - CONTROLLERS
+    static let factoryID: String = "purchasebleFactory_cell"
+    
+    // Selected Factory control
+    private(set) var selectedFactory: Factory? = nil
+    private(set) var selectedFactoryIndex: Int = -1 // Index of the cell
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // Design components
+        mainCurrencyHeaderView.layer.cornerRadius = 10
+        premiumCurrencyHeaderView.layer.cornerRadius = 10
+        sellAItemButton.layer.cornerRadius = 10
+        myAnnouncesButton.layer.cornerRadius = 10
+        
+        // Setting text
+        marketplaceHeaderLabel.text = NSLocalizedString("MarketplaceHeaderLabel", comment: "")
+        itemTypeSelector.setTitle(NSLocalizedString("BasicSelectorItem", comment: ""), forSegmentAt: 0)
+        itemTypeSelector.setTitle(NSLocalizedString("PremiumSelectorItem", comment: ""), forSegmentAt: 1)
+        sellAItemButton.setTitle(NSLocalizedString("SellAItemButton", comment: ""), for: .normal)
+        myAnnouncesButton.setTitle(NSLocalizedString("MyAnnouncesButton", comment: ""), for: .normal)
+        loadPlayerCurrencies()
+    }
+    
+    
+    // MARK: - ACTIONS
+    /**
+     Close Marketplace scene.
+     */
+    @IBAction func closeMarketplace(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
     }
     
     
     /**
-     Create the scene background.
+     Load player actual currencies value.
      */
-    func createBackground() -> SKShapeNode {
-        sceneBackground = SKShapeNode(rect: CGRect(x: 0, y: 0, width: (GameScene.deviceScreenWidth), height: (GameScene.deviceScreenHeight)))
-        sceneBackground.fillColor = UIColor(named: "HudActions-background")!
-        sceneBackground.zPosition = 10
-        return sceneBackground
+    func loadPlayerCurrencies() {
+        mainCurrencyLabel.text = doubleToString(value: GameScene.user?.mainCurrency ?? 0.0)
+        premiumCurrencyLabel.text = doubleToString(value: GameScene.user?.premiumCurrency ?? 0.0)
+    }
+    
+}
+
+
+// MARK: - COLLECTIONVIEW DATASOURCE
+extension GameMarketplaceSceneController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
     }
     
     
-    /**
-     Create the button to close the actual scene.
-     */
-    func createCloseButton() -> SKSpriteNode {
-        let closeAction = SKSpriteNode(color: .white, size: CGSize(width: 90, height: 90))
-        closeAction.name = "CloseMarketplaceScene"
-        closeAction.zPosition = 11
-        return closeAction
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        #warning("CHANGE FROM WHERE IT PULLS")
+        let generatorsSize = (GameScene.user?.generators.count)!
+        
+        if indexPath.row >= generatorsSize {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Self.factoryID, for: indexPath) as! GameMarketplaceViewCell
+            cell.pullMarketplaceFactories(texture: "", resources: [])
+            cell.configureCell()
+
+            return cell
+        } else {
+            #warning("CHANGE FROM WHERE IT PULLS")
+            let generator = GameScene.user?.generators[indexPath.row]
+            let generatorResources = (generator?.resourcesArray)!
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Self.factoryID, for: indexPath) as! GameMarketplaceViewCell
+
+            if generator?.isActive == .no {
+                cell.pullMarketplaceFactories(texture: generator!.textureName, resources: generatorResources)
+                cell.configureCell()
+            } else {
+                cell.pullMarketplaceFactories(texture: generator!.textureName, resources: generatorResources)
+                cell.configureCell()
+            }
+            return cell
+        }
     }
+}
+
+
+// MARK: - COLLECTIONVIEW DELEGATE
+extension GameMarketplaceSceneController: UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? GameMarketplaceViewCell else { return }
+                cell.layer.borderWidth = 0
+                cell.layer.borderColor = UIColor.black.cgColor
+                cell.layer.cornerRadius = 10
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) as? GameMarketplaceViewCell else { return }
+                cell.layer.borderWidth = 2
+                cell.layer.borderColor = UIColor.black.cgColor
+                cell.layer.cornerRadius = 10
+        
+        #warning("SELECTED DATA FROM MARKETPLACE??")
+        guard let factory = GameScene.user?.generators else {
+            return
+        }
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize = CGSize(width: 131, height: 201)
+        return cellSize
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
 }
