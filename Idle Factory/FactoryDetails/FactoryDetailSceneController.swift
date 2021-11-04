f98706668bed726c7182ee46db27a1825a1c585f
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-11-04 15:24:43 -0300

    background color

--- Idle Factory/FactoryDetails/FactoryDetailSceneController.swift
+++ Idle Factory/FactoryDetails/FactoryDetailSceneController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class FactoryDetailSceneController: UIViewController,  UITableViewDataSource, UITableViewDelegate {
-   
+    
+    @IBOutlet var mainView: UIView!
     @IBOutlet weak var view2: UIView!
     
-    var generatorID: Int = 0
+    static var generator: Factory? = nil
+    static var isBlue: Bool = false
     
     @IBOutlet weak var id: UILabel!
     @IBOutlet weak var purchaseButton: UIButton!
@@ -28,6 +30,13 @@ class FactoryDetailSceneController: UIViewController,  UITableViewDataSource, UI
         tableView.dataSource = self
         tableView.delegate = self
         
+        
+        if FactoryDetailSceneController.isBlue {
+            mainView.backgroundColor = UIColor(named: "HudActions-background")
+        }
+        else {
+            mainView.backgroundColor = UIColor(named: "Marketplace_background")
+        }
         cancelButton.layer.cornerRadius = 10
         purchaseButton.layer.cornerRadius = 10
         
@@ -39,43 +48,58 @@ class FactoryDetailSceneController: UIViewController,  UITableViewDataSource, UI
         
         cancelButton.backgroundColor = UIColor.white
         purchaseButton.backgroundColor = UIColor(named: "Inventory_background")
-        
-        id.text = GameScene.user?.generators[generatorID].id
-        var resourceArray: [Resource] = []
-        var price = 0.0
-        for n in 0..<(GameScene.user?.generators[generatorID].resourcesArray.count ?? 0) {
-            resourceArray.append((GameScene.user?.generators[generatorID].resourcesArray[n])!)
-            price += resourceArray[n].basePrice
+        if let generator = FactoryDetailSceneController.generator {
+            id.text = generator.id
+            var resourceArray: [Resource] = []
+            var price = 0.0
+            for n in 0..<(generator.resourcesArray.count) {
+                resourceArray.append((generator.resourcesArray[n]))
+                price += resourceArray[n].basePrice
+            }
+            
+            priceLabel.text = "\(NSLocalizedString("Price", comment: "Price")) "
+            if generator.type == .NFT {
+                priceImage.image = UIImage(named: "Money_premium")
+            }
+            else {
+                priceImage.image = UIImage(named: "Coin")
+            }
+#warning("Trocar por valor da Offer")
+            priceValue.text = "1000"
         }
         
-        priceLabel.text = "\(NSLocalizedString("Price", comment: "Price")) "
         //view2.layer.borderColor = CGColor()
     }
-
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return GameScene.user?.generators[generatorID].resourcesArray.count ?? 1;
+        if let generator = FactoryDetailSceneController.generator {
+            return generator.resourcesArray.count;
+        }
+        return 0
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FactoryDetailCell", for: indexPath) as! FactoryDetailCell
-        if let resource =  GameScene.user?.generators[generatorID].resourcesArray[indexPath.row] {
+        if let generator = FactoryDetailSceneController.generator {
+            let resource =  generator.resourcesArray[indexPath.row]
             let qtd = (resource.qttPLevel * Double(resource.currentLevel)) + resource.baseQtt
             cell.name.text = "\(doubleToString(value: qtd)) \(resource.type.description)/s"
             cell.price.text = "\(doubleToString(value: resource.perSec))/s"
             cell.imageInCell.image = UIImage(named: getResourceImageName(resource: resource.type))
             
+            
         }
         return cell
     }
     
-  
-    
-//    @IBAction func close(_ sender: Any) {
-//        self.dismiss(animated: false, completion: nil)
-//    }
     
     
-   
+    //    @IBAction func close(_ sender: Any) {
+    //        self.dismiss(animated: false, completion: nil)
+    //    }
+    
+    
+    
 }
