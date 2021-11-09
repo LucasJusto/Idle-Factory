ea1562af465ff617bf2b29c6eb84ce462773242a
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2021-11-09 16:25:48 -0300

    add getComponent method to baseBig enum to get the type from a string.

--- Idle Factory/Base/FactoryVisualGenerator.swift
+++ Idle Factory/Base/FactoryVisualGenerator.swift
@@ -525,7 +525,65 @@ enum BaseBigRelatedPositions: CustomStringConvertible {
                 return "chimney3"
             case .chimney4:
                 return "chimney4"
-                
+        }
+    }
+    
+    static func getComponent(key: String) -> BaseBigRelatedPositions {
+        switch key {
+            case "window1_left":
+                return .windowLeft1
+            case "window2_left":
+                return .windowLeft2
+            case "window3_left":
+                return .windowLeft3
+            case "window1_right":
+                return .windowRight1
+            case "window2_right":
+                return .windowRight2
+            case "window3_right":
+                return .windowRight3
+            case "window1_big_left":
+                return .windowBigLeft1
+            case "window2_big_left":
+                return .windowBigLeft2
+            case "window3_big_left":
+                return .windowBigLeft3
+            case "window1_big_right":
+                return .windowBigRight1
+            case "window2_big_right":
+                return .windowBigRight2
+            case "window3_big_right":
+                return .windowBigRight3
+            case "door_big_left":
+                return .doorBigLeft
+            case "door_big_right":
+                return .doorBigRight
+            case "door_garage_left":
+                return .doorGarageLeft
+            case "door_garage_right":
+                return .doorGarageRight
+            case "door1_small_left":
+                return .doorSmallLeft1
+            case "door2_small_left":
+                return .doorSmallLeft2
+            case "door3_small_left":
+                return .doorSmallLeft3
+            case "door1_small_right":
+                return .doorSmallRight1
+            case "door2_small_right":
+                return .doorSmallRight2
+            case "door3_small_right":
+                return .doorSmallRight3
+            case "chimney1":
+                return .chimney1
+            case "chimney2":
+                return .chimney2
+            case "chimney3":
+                return .chimney3
+            case "chimney4":
+                return .chimney4
+            default:
+                return .chimney1
         }
     }
     
