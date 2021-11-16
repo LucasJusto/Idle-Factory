863ce63ff459dff8f5e4d9200131baa6849cf621
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-11-16 17:35:08 -0300

    onboarding

---
+++ Idle Factory/Onboarding /FirstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  Idle Factory
+//
+//  Created by Felipe Grosze Nipper de Oliveira on 16/11/21.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var Label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Label1.text = NSLocalizedString("Adquira fábricas e evolua para aumentar o seu parque industrial", comment: "")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
